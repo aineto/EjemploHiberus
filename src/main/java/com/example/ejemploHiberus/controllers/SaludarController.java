61365dc673ae8ec60166acd670c880924a55fa51
Usuario <Usuario@host.docker.internal>
2021-09-28 09:58:07 +0200

    Merge origin/master
    Conflicts:
    	HiberLibros/src/main/resources/templates/principal/buscarRelatos.html

--- src/main/java/com/example/ejemploHiberus/controllers/SaludarController.java
+++ src/main/java/com/example/ejemploHiberus/controllers/SaludarController.java
@@ -5,8 +5,13 @@
  */
 package com.example.ejemploHiberus.controllers;
 
+import entities.Persona;
+import java.util.List;
+import java.util.UUID;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import repositories.PersonaRepository;
 
 /**
  *
@@ -14,11 +19,21 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class SaludarController {
-    
-    
+
+    @Autowired
+    private PersonaRepository repo;
+
     @GetMapping("/saludar")
-    public String saludar(){
+    public String saludar() {
         return "Hola mundo";
     }
-    
+
+    @GetMapping("/add")
+    public List<Persona> add() {
+        Persona p = new Persona();
+        p.setNombre(UUID.randomUUID().toString());
+        repo.save(p);
+        return repo.findAll();
+    }
+
 }
